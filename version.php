1701286c473d5ecf1230dd4997a2e312c93e6537
Andrew Zito <zitoa@lafayette.edu>
2018-07-19 22:03:10 -0400

    Version for Moodle 3.5

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018042700;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018042735;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017051500;        // Requires this Moodle version.
 $plugin->component = 'local_hide_inactive_courses';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release   = '1.0.5';
+$plugin->release   = '1.0.5-35';
