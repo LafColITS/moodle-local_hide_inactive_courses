83b8d05f4fc7e29153b9cc35b809b62c75c33aab
Andrew Zito <zitoa@lafayette.edu>
2018-11-26 14:20:41 -0500

    Fix version number

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018072500;                     // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018112600;                     // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018112500;                     // Requires this Moodle version.
 $plugin->component = 'local_hide_inactive_courses';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_ALPHA;
